47ca26661abeb8343f5c71137e87beefb63086fd
uzayilica <uzayilica2@gmail.com>
2024-09-16 07:38:14 +0300

    tets

---
+++ src/main/java/org/yemekler/reactiv/ReactivApplication.java
@@ -0,0 +1,17 @@
+package org.yemekler.reactiv;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ReactivApplication {
+
+    public static void main(String[] args) {
+        System.out.println("başlangıç");
+
+    }}
---
+++ src/test/java/org/yemekler/reactiv/ReactivApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yemekler.reactiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
